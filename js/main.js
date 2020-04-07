6c44394af69d2f51718441b40982333789b858b3
Maksym Vintskovskyi <intadmirmaks@gmail.com>
2020-04-07 11:14:17 +0300

    First com

--- js/main.js
+++ js/main.js
@@ -36,9 +36,10 @@ $(document).ready(function(){
  
       // :::::::::::: Search :::::::::::::
   $('.search').on('click', function(){
-    $('.search-input').show();
+    $('.search-input').show().focus();
     $('.search button').show();
     $('.search-img').hide();
+    // $('.search-input).focus();
   })
   $(document).on('click', function(e){
     if(e.target != $('.search')[0] && $('.search').has(e.target).length === 0 ){
@@ -52,12 +53,88 @@ $(document).ready(function(){
     $('.navigation-mob').hide();
   })
   
-  $(".tab-content").not(":first").hide();
+  // $(".tab-content").not(":first").hide();
   $(".card-tab").click(function() {
     $(".card-tab").removeClass("active").eq($(this).index()).addClass("active");
-    $(".tab-content").hide().eq($(this).index()).fadeIn();
+    // $(".tab-content").hide().eq($(this).index()).fadeIn();
   }).eq(0).addClass("active");
- 
+
+  // Select
+	$('.select').each(function(){
+		// Variables
+		var $this = $(this),
+			selectOption = $this.find('option'),
+			selectOptionLength = selectOption.length,
+			selectedOption = selectOption.filter(':selected'),
+			dur = 300;
+
+		$this.hide();
+		// Wrap all in select box
+		$this.wrap('<div class="select"></div>');
+		// Style box
+		$('<div>',{
+			class: 'select__gap',
+			text: 'Select'
+		}).insertAfter($this);
+		
+		var selectGap = $this.next('.select__gap'),
+			caret = selectGap.find('.caret');
+		// Add ul list
+		$('<ul>',{
+			class: 'select__list'
+		}).insertAfter(selectGap);		
+
+		var selectList = selectGap.next('.select__list');
+		// Add li - option items
+		for(var i = 0; i < selectOptionLength; i++){
+			$('<li>',{
+				class: 'select__item',
+				html: $('<span>',{
+					text: selectOption.eq(i).text()
+				})				
+			})
+			.attr('data-value', selectOption.eq(i).val())
+			.appendTo(selectList);
+		}
+		// Find all items
+		var selectItem = selectList.find('li');
+
+		selectList.slideUp(0);
+		selectGap.on('click', function(){
+			if(!$(this).hasClass('on')){
+				$(this).addClass('on');
+				selectList.slideDown(dur);
+
+				selectItem.on('click', function(){
+					var chooseItem = $(this).data('value');
+
+					$('select').val(chooseItem).attr('selected', 'selected');
+					selectGap.text($(this).find('span').text());
+
+					selectList.slideUp(dur);
+					selectGap.removeClass('on');
+				});
+				
+			} else {
+				$(this).removeClass('on');
+				selectList.slideUp(dur);
+			}
+		});		
+  });
+  
+  $(".delivery-dropdown").niceScroll({
+    cursorwidth:"2px",
+    cursorborder: "2px solid #fff",
+    autohidemode: false,
+    cursorborderradius: "1px",
+    railpadding: 
+    {
+      top: 20, 
+      right: 4,
+      left: 0,
+      bottom: 20
+    }
+});
       // :::::::::::: Menu :::::::::::::
   
 
