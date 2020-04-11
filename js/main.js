1b7718f5eb112efbe5e8b8b4a665ecdc986f0240
Maksym Vintskovskyi <intadmirmaks@gmail.com>
2020-04-12 00:20:07 +0300

    First com

--- js/main.js
+++ js/main.js
@@ -54,67 +54,67 @@ $(document).ready(function(){
   });
 
   // Select
-	$('.select').each(function(){
-		// Variables
-		var $this = $(this),
-			selectOption = $this.find('option'),
-			selectOptionLength = selectOption.length,
-			selectedOption = selectOption.filter(':selected'),
-			dur = 300;
+	// $('.select').each(function(){
+	// 	// Variables
+	// 	var $this = $(this),
+	// 		selectOption = $this.find('option'),
+	// 		selectOptionLength = selectOption.length,
+	// 		selectedOption = selectOption.filter(':selected'),
+	// 		dur = 300;
 
-		$this.hide();
-		// Wrap all in select box
-		$this.wrap('<div class="select"></div>');
-		// Style box
-		$('<div>',{
-			class: 'select__gap',
-			text: 'Select'
-		}).insertAfter($this);
+	// 	$this.hide();
+	// 	// Wrap all in select box
+	// 	$this.wrap('<div class="select"></div>');
+	// 	// Style box
+	// 	$('<div>',{
+	// 		class: 'select__gap',
+	// 		text: 'Select'
+	// 	}).insertAfter($this);
 		
-		var selectGap = $this.next('.select__gap'),
-			caret = selectGap.find('.caret');
-		// Add ul list
-		$('<ul>',{
-			class: 'select__list'
-		}).insertAfter(selectGap);		
+	// 	var selectGap = $this.next('.select__gap'),
+	// 		caret = selectGap.find('.caret');
+	// 	// Add ul list
+	// 	$('<ul>',{
+	// 		class: 'select__list'
+	// 	}).insertAfter(selectGap);		
 
-		var selectList = selectGap.next('.select__list');
-		// Add li - option items
-		for(var i = 0; i < selectOptionLength; i++){
-			$('<li>',{
-				class: 'select__item',
-				html: $('<span>',{
-					text: selectOption.eq(i).text()
-				})				
-			})
-			.attr('data-value', selectOption.eq(i).val())
-			.appendTo(selectList);
-		}
-		// Find all items
-		var selectItem = selectList.find('li');
+	// 	var selectList = selectGap.next('.select__list');
+	// 	// Add li - option items
+	// 	for(var i = 0; i < selectOptionLength; i++){
+	// 		$('<li>',{
+	// 			class: 'select__item',
+	// 			html: $('<span>',{
+	// 				text: selectOption.eq(i).text()
+	// 			})				
+	// 		})
+	// 		.attr('data-value', selectOption.eq(i).val())
+	// 		.appendTo(selectList);
+	// 	}
+	// 	// Find all items
+	// 	var selectItem = selectList.find('li');
 
-		selectList.slideUp(0);
-		selectGap.on('click', function(){
-			if(!$(this).hasClass('on')){
-				$(this).addClass('on');
-				selectList.slideDown(dur);
+	// 	selectList.slideUp(0);
+	// 	selectGap.on('click', function(){
+	// 		if(!$(this).hasClass('on')){
+	// 			$(this).addClass('on');
+	// 			selectList.slideDown(dur);
 
-				selectItem.on('click', function(){
-					var chooseItem = $(this).data('value');
+	// 			selectItem.on('click', function(){
+	// 				var chooseItem = $(this).data('value');
 
-					$('select').val(chooseItem).attr('selected', 'selected');
-					selectGap.text($(this).find('span').text());
+	// 				$('select').val(chooseItem).attr('selected', 'selected');
+	// 				selectGap.text($(this).find('span').text());
 
-					selectList.slideUp(dur);
-					selectGap.removeClass('on');
-				});
+	// 				selectList.slideUp(dur);
+	// 				selectGap.removeClass('on');
+	// 			});
 				
-			} else {
-				$(this).removeClass('on');
-				selectList.slideUp(dur);
-			}
-		});		
-  });
+	// 		} else {
+	// 			$(this).removeClass('on');
+	// 			selectList.slideUp(dur);
+	// 		}
+	// 	});		
+  // });
   
   $(".delivery-dropdown").niceScroll({
     cursorwidth:"2px",
@@ -153,4 +153,12 @@ function hide_search(){
 }
 function close_cab(){
   $('.cab-dropdown').hide();
+}
+function close_modal(e){
+  e.preventDefault();
+  $('.modal-change').hide(200);
+}
+function open_modal(event){
+  // event.preventDefault();
+  $('.modal-change').show(200);
 }
