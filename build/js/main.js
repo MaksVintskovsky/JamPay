98f1f14c1199a8eda7f05011ec78fc848c98703c
Maksym Vintskovskyi <intadmirmaks@gmail.com>
2020-04-15 21:40:22 +0300

    corrections

--- build/js/main.js
+++ build/js/main.js
@@ -150,90 +150,35 @@ $(document).ready(function(){
     $('.navigation-mob').hide();
   });
 
-  // Select
-	// $('.select').each(function(){
-	// 	// Variables
-	// 	var $this = $(this),
-	// 		selectOption = $this.find('option'),
-	// 		selectOptionLength = selectOption.length,
-	// 		selectedOption = selectOption.filter(':selected'),
-	// 		dur = 300;
 
-	// 	$this.hide();
-	// 	// Wrap all in select box
-	// 	$this.wrap('<div class="select"></div>');
-	// 	// Style box
-	// 	$('<div>',{
-	// 		class: 'select__gap',
-	// 		text: 'Select'
-	// 	}).insertAfter($this);
-		
-	// 	var selectGap = $this.next('.select__gap'),
-	// 		caret = selectGap.find('.caret');
-	// 	// Add ul list
-	// 	$('<ul>',{
-	// 		class: 'select__list'
-	// 	}).insertAfter(selectGap);		
 
-	// 	var selectList = selectGap.next('.select__list');
-	// 	// Add li - option items
-	// 	for(var i = 0; i < selectOptionLength; i++){
-	// 		$('<li>',{
-	// 			class: 'select__item',
-	// 			html: $('<span>',{
-	// 				text: selectOption.eq(i).text()
-	// 			})				
-	// 		})
-	// 		.attr('data-value', selectOption.eq(i).val())
-	// 		.appendTo(selectList);
-	// 	}
-	// 	// Find all items
-	// 	var selectItem = selectList.find('li');
+  // :::::::::::: Popup :::::::::::::
 
-	// 	selectList.slideUp(0);
-	// 	selectGap.on('click', function(){
-	// 		if(!$(this).hasClass('on')){
-	// 			$(this).addClass('on');
-	// 			selectList.slideDown(dur);
-
-	// 			selectItem.on('click', function(){
-	// 				var chooseItem = $(this).data('value');
-
-	// 				$('select').val(chooseItem).attr('selected', 'selected');
-	// 				selectGap.text($(this).find('span').text());
-
-	// 				selectList.slideUp(dur);
-	// 				selectGap.removeClass('on');
-	// 			});
-				
-	// 		} else {
-	// 			$(this).removeClass('on');
-	// 			selectList.slideUp(dur);
-	// 		}
-	// 	});		
+  // $('.btn_group .btn_gray').on('click', function(e){
+  //   e.preventDefault();
+  //   $('.modal-change').hide(200);
   // });
+
   
-  $(".delivery-dropdown").niceScroll({
-    cursorwidth:"2px",
-    cursorborder: "2px solid #fff",
-    autohidemode: false,
-    cursorborderradius: "1px",
-    railpadding: 
-    {
-      top: 20, 
-      right: 4,
-      left: 0,
-      bottom: 20
-    }
-});
-      
+  // $(".delivery-dropdown").niceScroll({
+  //   cursorwidth:"2px",
+  //   cursorborder: "2px solid #fff",
+  //   autohidemode: false,
+  //   cursorborderradius: "1px",
+  //   railpadding: 
+  //   {
+  //     top: 20, 
+  //     right: 4,
+  //     left: 0,
+  //     bottom: 20
+  //   }
+  // });
+        
   
 
-    // :::::::::::: Popup :::::::::::::
+    
   
     // :::::::::::: Filter :::::::::::::
-  
-
     // :::::::::::: Catalog :::::::::::::
     
 });
@@ -247,15 +192,15 @@ function hide_search(){
   $('.search-input').show().focus();
   $('.search button').show();
   $('.search-img').hide();
-}
+};
 function close_cab(){
   $('.cab-dropdown').hide();
-}
-function close_modal(e){
-  e.preventDefault();
-  $('.modal-change').hide(200);
-}
+};
+function close_modal_change(e){
+  // e.preventDefault();
+  $('#my_fucking_modal').hide(200);
+};
 function open_modal(event){
   // event.preventDefault();
   $('.modal-change').show(200);
-}
+};
